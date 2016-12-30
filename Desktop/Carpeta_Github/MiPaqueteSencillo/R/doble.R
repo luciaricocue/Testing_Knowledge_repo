49791ec8bc448c46fa33e8c084ca3f018c0a6d61
luciaricocue@gmail.com <luciaricocue@gmail.com>
2016-12-30 13:14:03 +0100

    subo archivos a GitHub

---
+++ Desktop/Carpeta_Github/MiPaqueteSencillo/R/doble.R
@@ -0,0 +1,11 @@
+
+#'Calcula el doble de un número, es decir, lo multiplica por 2
+#'
+#'@param x número
+#'@return El doble de \code{x}
+#'@examples
+#'doble(7)
+#'doble(8)
+#'@export
+
+doble <- function(x) 2 * x
---
+++ Desktop/Carpeta_Github/MiPaqueteSencillo/R/suma.dos.numeros.R
@@ -0,0 +1,13 @@
+#'Suma dos números
+#'
+#'@param x número
+#'@param y número
+#'@return La suma de \code{x} y \code{y}
+#'@examples
+#'suma.dos.numeros(2, 7)
+#'suma.dos.numeros(3, 8)
+#'@export
+
+suma.dos.numeros <- function(x, y){
+  x + y
+}
